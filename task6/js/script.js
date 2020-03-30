5de98ab458ad9633ae99bf50590010e1c422fda1
Nikita Dronov <nikoman007@gmail.com>
2020-03-30 18:20:15 +0400

    Фикс задания 6

--- task6/js/script.js
+++ task6/js/script.js
@@ -1,14 +1,41 @@
+"use strict";
 let body = document.getElementsByTagName("body")[0],
-  menu = document.querySelector(".menu"),
-  ul_menu_item = document.createElement("div"),
-  title = document.getElementById("title"),
   adv = document.querySelectorAll(".adv"),
   divPrompt = document.getElementById("prompt");
 
+// Восстановить порядок в меню, добавить пятый пункт
+const menu = document.querySelector(".menu"),
+  ul_menu_items_list = document.querySelectorAll(".menu-item"),
+  ul_menu_items_arr = Array.prototype.slice.call(ul_menu_items_list),
+  ul_menu_item = document.createElement("div");
+
+ul_menu_items_arr.sort((a, b) => {
+  const order = [
+    "Первый пункт",
+    "Второй пункт",
+    "Третий пункт",
+    "Четвертый пункт"
+  ];
+
+  const index_a = order.indexOf(a.textContent),
+    index_b = order.indexOf(b.textContent);
+
+  return index_a - index_b;
+});
+ul_menu_items_arr.forEach(el => menu.appendChild(el));
 ul_menu_item.classList.add("menu-item");
 ul_menu_item.textContent = "Пятый пункт";
 menu.appendChild(ul_menu_item);
-body.style.background = "url(../img/apple_true.jpg) center no-repeat";
+
+// Заменить картинку заднего фона на другую из папки img
+document.body.style.background = "url(../img/apple_true.jpg) center no-repeat";
+
+// Поменять заголовок, добавить слово "подлинную"
+const title = document.getElementById("title");
 title.textContent = title.textContent.replace("только", "подлинную");
-adv.forEach(el => el.remove());
+
+// Удалить рекламу со страницы
+document.querySelectorAll(".adv").forEach(el => el.remove());
+
+// Спросить у пользователя отношение к технике apple и записать ответ в блок на странице с id "prompt"
 divPrompt.textContent = prompt("Ваше отношение к технике apple?", "");
